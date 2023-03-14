922f3e17cc2bdf592852ccacb5293038e7dc5b63
조해성 <jhsv8212@jobkorea.co.kr>
2023-03-14 18:01:13 +0900

    jwt 관련 작업

---
+++ src/main/java/com/review/api/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package com.review.api.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+// jwt가 정상적인 토큰 일 경우 권한이 없는 리소스에 접근할 때 발생하는 예외 처리
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException,
+            ServletException {
+        response.sendRedirect("/exception/accessdenied");
+    }
+}
---
+++ src/main/java/com/review/api/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.review.api.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException,
+            ServletException {
+        response.sendRedirect("/exception/entrypoint");
+    }
+}
